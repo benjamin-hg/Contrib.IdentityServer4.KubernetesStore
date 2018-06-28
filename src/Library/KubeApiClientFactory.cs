9d41b1db8bbf8d2016946d2229f322d72172b0a7
Andreas Bieber <andreas.bieber@axoom.com>
2018-06-28 08:20:05 +0200

    Adds logging to KubeApiClientFactory

--- src/Library/KubeApiClientFactory.cs
+++ src/Library/KubeApiClientFactory.cs
@@ -1,20 +1,35 @@
 ﻿using KubeClient;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Contrib.IdentityServer4.KubernetesStore
 {
     public class KubeApiClientFactory
     {
+        private readonly ILogger<KubeApiClientFactory> _logger;
         private readonly string _connectionString;
 
-        public KubeApiClientFactory(IOptions<KubernetesConfigurationStoreOptions> options)
+        public KubeApiClientFactory(ILogger<KubeApiClientFactory> logger, IOptions<KubernetesConfigurationStoreOptions> options)
         {
+            _logger = logger;
             _connectionString = options.Value.ConnectionString;
         }
 
         public IKubeApiClient Build()
-            => string.IsNullOrWhiteSpace(_connectionString)
-                   ? KubeApiClient.CreateFromPodServiceAccount()
-                   : KubeApiClient.Create(_connectionString);
+        {
+            KubeClientOptions options;
+            if (string.IsNullOrWhiteSpace(_connectionString))
+            {
+                options = KubeClientOptions.FromPodServiceAccount();
+                _logger.LogInformation("Using cluster-internal kubernetes connection.");
+            }
+            else
+            {
+                options = new KubeClientOptions(_connectionString);
+                _logger.LogInformation("Using remote kubernetes connection.");
+            }
+
+            return KubeApiClient.Create(options);
+        }
     }
 }
