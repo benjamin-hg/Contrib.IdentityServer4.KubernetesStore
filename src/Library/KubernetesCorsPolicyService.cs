49803ff9bd998be23f720e0d4051b1e824098a4e
Benjamin Hünig <benjamin.huenig@axoom.com>
2018-10-23 18:41:16 +0200

    Filters out clients that cause the CORS checks in base class to throw

--- src/Library/KubernetesCorsPolicyService.cs
+++ src/Library/KubernetesCorsPolicyService.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Contrib.KubeClient.CustomResources;
+using IdentityServer4.Models;
 using IdentityServer4.Services;
 using Microsoft.Extensions.Logging;
 
@@ -10,7 +12,21 @@ namespace Contrib.IdentityServer4.KubernetesStore
     public class KubernetesCorsPolicyService : InMemoryCorsPolicyService
     {
         public KubernetesCorsPolicyService(ILogger<KubernetesCorsPolicyService> logger, ICustomResourceWatcher<ClientResource> clientWatcher)
-            : base(logger, clientWatcher.RawResources.Select(resource => resource.Spec))
-        {}
+            : base(logger, clientWatcher.RawResources.Select(resource => resource.Spec).Where(client => HasWellFormedAllowedCorsOrigins(client, logger)))
+        { }
+
+        private static bool HasWellFormedAllowedCorsOrigins(Client client, ILogger logger)
+        {
+            var hasWellFormedAllowedCorsOrigins = client.AllowedCorsOrigins.All(origin => Uri.IsWellFormedUriString(origin, UriKind.RelativeOrAbsolute));
+            if (hasWellFormedAllowedCorsOrigins)
+            {
+                return true;
+            }
+            else
+            {
+                logger.LogCritical($"Identity Client with name {client.ClientName} has invalid AllowedCorsOrigins");
+                return false;
+            }
+        }
     }
 }
