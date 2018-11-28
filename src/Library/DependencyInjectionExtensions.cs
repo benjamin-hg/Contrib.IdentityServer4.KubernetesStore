4c1a3a8a357be44ce6863bcb8fbc46b35400c829
Nils Engelbach <nils.engelbach@axoom.com>
2018-11-28 15:44:03 +0100

    Adds support for identity resources

--- src/Library/DependencyInjectionExtensions.cs
+++ src/Library/DependencyInjectionExtensions.cs
@@ -20,27 +20,29 @@ namespace Contrib.IdentityServer4.KubernetesStore
         /// </summary>
         public static IServiceCollection AddIdentityResourceClients(this IServiceCollection services)
             => services.AddCustomResourceClient<ClientResource>()
-                       .AddCustomResourceClient<ApiResourceResource>();
+                       .AddCustomResourceClient<ApiResourceResource>()
+                       .AddCustomResourceClient<IdentityResourceResource>();
 
         /// <summary>
         /// Adds <see cref="ICustomResourceWatcher{TResource}"/>s for <see cref="ClientResource"/>s and <see cref="ApiResourceResource"/>s.
         /// </summary>
         public static IServiceCollection AddIdentityResourceWatchers(this IServiceCollection services)
             => services.AddCustomResourceWatcher<ClientResource>()
-                       .AddCustomResourceWatcher<ApiResourceResource>();
+                       .AddCustomResourceWatcher<ApiResourceResource>()
+                       .AddCustomResourceWatcher<IdentityResourceResource>();
 
         /// <summary>
         /// Adds <see cref="IClientStore"/>, <see cref="IResourceStore"/> and <see cref="ICorsPolicyService"/> implementations backed by Kubernetes Custom Resources.
         /// </summary>
         /// <param name="services">The service collection.</param>
-        /// <param name="identityResources"><see cref="IdentityResource"/> to be used. Default is an empty list.</param>
-        public static IServiceCollection AddIdentityKubernetesStores(this IServiceCollection services, params IdentityResource[] identityResources)
+        /// <param name="defaultIdentityResources"><see cref="IdentityResource"/> to be used. Default is an empty list.</param>
+        public static IServiceCollection AddIdentityKubernetesStores(this IServiceCollection services, params IdentityResource[] defaultIdentityResources)
         {
             services.AddIdentityResourceWatchers()
                     .AddSingleton<IClientStore, KubernetesClientStore>()
                     .AddSingleton<IResourceStore, KubernetesResourceStore>()
                     .AddSingleton<ICorsPolicyService, KubernetesCorsPolicyService>()
-                    .TryAddSingleton(identityResources.AsEnumerable());
+                    .TryAddSingleton(defaultIdentityResources.AsEnumerable());
             return services;
         }
 
@@ -48,10 +50,10 @@ namespace Contrib.IdentityServer4.KubernetesStore
         /// Adds <see cref="IClientStore"/>, <see cref="IResourceStore"/> and <see cref="ICorsPolicyService"/> implementations backed by Kubernetes Custom Resources.
         /// </summary>
         /// <param name="builder">The builder.</param>
-        /// <param name="identityResources"><see cref="IdentityResource"/> to be used. Default is an empty list.</param>
-        public static IIdentityServerBuilder AddKubernetesConfigurationStore(this IIdentityServerBuilder builder, params IdentityResource[] identityResources)
+        /// <param name="defaultIdentityResources"><see cref="IdentityResource"/> to be used. Default is an empty list.</param>
+        public static IIdentityServerBuilder AddKubernetesConfigurationStore(this IIdentityServerBuilder builder, params IdentityResource[] defaultIdentityResources)
         {
-            builder.Services.AddIdentityKubernetesStores(identityResources);
+            builder.Services.AddIdentityKubernetesStores(defaultIdentityResources);
             return builder;
         }
     }
---
+++ src/Library/IdentityResourceResource.cs
@@ -0,0 +1,28 @@
+using Contrib.KubeClient.CustomResources;
+using IdentityServer4.Models;
+using KellermanSoftware.CompareNetObjects;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Contrib.IdentityServer4.KubernetesStore
+{
+    public class IdentityResourceResource : CustomResource<IdentityResource>, IPayloadPatchable<IdentityResourceResource>
+    {
+        public new static CustomResourceDefinition Definition { get; } = Crd.For(pluralName: "identityidresources", kind: "IdentityIdResource");
+
+        public IdentityResourceResource()
+            : base(Definition)
+        {}
+
+        public IdentityResourceResource(string @namespace, string name, IdentityResource spec)
+            : base(Definition, @namespace, name, spec)
+        {}
+
+        private static readonly CompareLogic SpecComparer = new CompareLogic();
+
+        protected override bool SpecEquals(IdentityResource other)
+            => SpecComparer.Compare(Spec, other).AreEqual;
+
+        public void ToPayloadPatch(JsonPatchDocument<IdentityResourceResource> patch)
+            => patch.Replace(x => x.Spec, Spec);
+    }
+}
