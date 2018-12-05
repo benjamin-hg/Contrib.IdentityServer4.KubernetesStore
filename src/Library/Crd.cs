cbcc90dbe6582c5957364e13afe985599f57c898
Benjamin Hünig <benjamin.huenig@axoom.com>
2018-12-05 15:45:32 +0100

    Removes "stable" from namespace "stable.contrib.identityserver.io"

--- src/Library/Crd.cs
+++ src/Library/Crd.cs
@@ -5,6 +5,6 @@ namespace Contrib.IdentityServer4.KubernetesStore
     internal static class Crd
     {
         public static CustomResourceDefinition For(string pluralName, string kind)
-            => new CustomResourceDefinition("stable.contrib.identityserver.io/v1", pluralName, kind);
+            => new CustomResourceDefinition("contrib.identityserver.io/v1", pluralName, kind);
     }
 }
