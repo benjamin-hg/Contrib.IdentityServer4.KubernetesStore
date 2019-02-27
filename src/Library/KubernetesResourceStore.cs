84b0fc9127b655a0170c30a921092ed5af5777c4
Bastian Eicher <bastian.eicher@axoom.com>
2019-02-27 14:53:08 +0100

    Derive Api/IdentityResource.spec.clientId from resource namespace and name

--- src/Library/KubernetesResourceStore.cs
+++ src/Library/KubernetesResourceStore.cs
@@ -11,8 +11,28 @@ namespace Contrib.IdentityServer4.KubernetesStore
     public class KubernetesResourceStore : DuplicateScopeFilteringInMemoryResourcesStore
     {
         public KubernetesResourceStore(ILogger<KubernetesResourceStore> logger, ICustomResourceWatcher<IdentityResourceResource> identityResourceWatcher, ICustomResourceWatcher<ApiResourceResource> apiResourceWatcher, IEnumerable<IdentityResource> defaultIdentityResources = null)
-            : base(logger, identityResourceWatcher.Select(resource => resource.Spec).Concat(defaultIdentityResources ?? Enumerable.Empty<IdentityResource>()), apiResourceWatcher.Select(resource => resource.Spec))
-        { }
+            : base(
+                logger,
+                identityResourceWatcher.Select(GetIdentityResource).Concat(defaultIdentityResources ?? Enumerable.Empty<IdentityResource>()),
+                apiResourceWatcher.Select(GetApiResource))
+        {}
 
+        private static IdentityResource GetIdentityResource(IdentityResourceResource resource)
+        {
+            var identityResource = resource.Spec;
+            if (string.IsNullOrEmpty(identityResource.Name))
+                identityResource.Name = resource.Metadata.Namespace + "-" + resource.Metadata.Name;
+
+            return identityResource;
+        }
+
+        private static ApiResource GetApiResource(ApiResourceResource resource)
+        {
+            var apiResource = resource.Spec;
+            if (string.IsNullOrEmpty(apiResource.Name))
+                apiResource.Name = resource.Metadata.Namespace + "-" + resource.Metadata.Name;
+
+            return apiResource;
+        }
     }
 }
